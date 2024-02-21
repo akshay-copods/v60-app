fd4bd883c930774adb77da20984940d753a2f578
DhruvCopods <rahulkonda@copods.co>
2024-02-21 22:45:53 +0530

    fix: tab width

--- App.js
+++ App.js
@@ -5,38 +5,40 @@ import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
 const Tab = createBottomTabNavigator();
+
 function MyTabs() {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Details" component={DetailsScreen} />
-    </Tab.Navigator>
-  );
+    return (
+        <Tab.Navigator>
+            <Tab.Screen name="Home" component={HomeScreen} />
+            <Tab.Screen name="Details" component={DetailsScreen} />
+        </Tab.Navigator>
+    );
 }
 function HomeScreen() {
-  return (
-    <View className="flex items-center justify-center">
-      <Text className="text-red-400">Home Screen</Text>
-    </View>
-  );
+    return (
+        <View className="flex items-center justify-center">
+            <Text className="text-red-400">Home Screen</Text>
+        </View>
+    );
 }
 function DetailsScreen() {
-  return (
-    <View className="flex items-center justify-center">
-      <Text className="text-red-400">Details Screen</Text>
-    </View>
-  );
+    return (
+        <View className="flex items-center justify-center">
+            <Text className="text-red-400">Details Screen</Text>
+        </View>
+    );
 }
 
 const Stack = createNativeStackNavigator();
 
 function App() {
-  return (
-    <NavigationContainer>
-      <MyTabs />
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <MyTabs />
+        </NavigationContainer>
+    );
 }
 
 export default App;
